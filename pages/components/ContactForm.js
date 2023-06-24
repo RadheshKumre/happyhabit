0480ebb8c69331db778a81ef386f73cef11672b9
radhesh <radhesh.kumre@atriina.com>
2023-06-24 12:24:34 +0530

    first commit

---
+++ pages/components/ContactForm.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic here
+    // You can use the form data (name, email, message) to send an email or save it in a database
+
+    // Clear form fields after submission
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
