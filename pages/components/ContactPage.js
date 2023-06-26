6ea062cff4cd80b6f83eee3a32c4c61b1cf38c37
radhesh <radhesh.kumre@atriina.com>
2023-06-26 19:50:22 +0530

    UI enhacements

--- pages/components/ContactPage.js
+++ pages/components/ContactPage.js
@@ -1,12 +1,35 @@
 import ContactForm from './ContactForm';
-
+import React from 'react';
+import styles from '../../styles/ContactPage.module.css'
 const ContactPage = () => {
   return (
-    <div>
+    <div className={styles.container} >
+      <div className={styles.container_contact_form}>
       <h1>Contact Us</h1>
-      <ContactForm />
+      <div className={styles.comman}>
+      <div className={styles.data}>
+    
+        <p>
+          Please use the contact form below to<br/>get in touch with us directly. Fill in the<br/>required details and select the relevant<br/>subject so that we can provide you<br/>with the best assistance.
+        </p>
+        <p>Address :</p>
+        <p>302, The Shri Platinum Mall,<br/>Jawahar Rd, Ghatkopar East,<br/>Mumbai Maharashtra - 400077</p>
+        <p>Get Into Tuch :</p>
+        <p>contact@happyhabit.club</p>
+        <p>+91 8898343385</p>
+
+      </div>
+      <div className={styles.form}>
+      <ContactForm/>
+      </div>
+      </div>
+
+      </div>
+        
     </div>
   );
 };
 
 export default ContactPage;
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import ContactPage from './components/ContactPage'
+import ContactPage from '../pages/components/ContactPage'
+// import style from '../styles/ContactPage.css'
 
 export default function Home() {
   return (
